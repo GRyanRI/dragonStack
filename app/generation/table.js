a75d58b50bc5ef64ca11f82b00df3fa591c3468a
GRyanRI <gregorywryan@gmail.com>
2019-05-08 14:25:08 -0400

    Added trait table & Get trait

--- app/generation/table.js
+++ app/generation/table.js
@@ -1,25 +1,21 @@
-// holds any methos that interact with db table
 const pool = require('../../databasePool');
 
 class GenerationTable {
-    static storeGeneration(generation) {
-        return new Promise((resolve, reject) => {
-            pool.query(
-                'INSERT INTO generation(expiration) VALUES($1) RETURNING id',
-                [generation.expiration],
-                (error, response) => {
-                    if (error) return reject(error);
-    
-                    const generationId = response.rows[0].id;
+  static storeGeneration(generation) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        'INSERT INTO generation(expiration) VALUES($1) RETURNING id',
+        [generation.expiration],
+        (error, response) => {
+          if (error) return reject(error);
+  
+          const generationId = response.rows[0].id;
 
-                    resolve({ generationId });
-
-                    return generationId;
-                }
-            );
-        })
-        
-    }
+          resolve({ generationId });
+        }
+      );
+    });
+  }
 }
 
 module.exports = GenerationTable;
